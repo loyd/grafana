2807d51d3801a6f8f647d86b4ffd878d1a1c5b79
Isabella Siu <isabella.siu@grafana.com>
2022-04-20 14:06:21 -0400

    remove hidden

--- packages/grafana-ui/src/components/Select/DropdownIndicator.tsx
+++ packages/grafana-ui/src/components/Select/DropdownIndicator.tsx
@@ -3,7 +3,6 @@ import { Icon } from '../Icon/Icon';
 
 interface DropdownIndicatorProps {
   isOpen: boolean;
-  hidden?: boolean;
 }
 
 export const DropdownIndicator: React.FC<DropdownIndicatorProps> = ({ isOpen }) => {
